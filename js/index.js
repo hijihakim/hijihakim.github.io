908c71cc2c1fdd7f11ef7084481d05b0a255cdf6
hijihakim <hijihakim7@gmail.com>
2019-07-09 01:11:22 +0700

    menanmbahkan json dan merubah html css javascript

--- js/index.js
+++ js/index.js
@@ -1,42 +1,45 @@
-//variable
-const collection = document.querySelector('.collection');
-const navbar = document.querySelector('nav');
-const contactBox = document.querySelector('.contact-box');
-var click = {contact:false};
+const overTopCollection = document.querySelector(".collection").offsetTop;
 
-//function 
-function toggle(element, event){
-	if (event == 'on'){
-		element.classList.add('on-block');
-		element.classList.remove('off');
+$(window).on('scroll', function(){
+	if(document.documentElement.scrollTop > overTopCollection){
+		$('.off').show(1000);
 	}else{
-		element.classList.add('off');
-		element.classList.remove('on-block');
+		$('.off').hide(1000);
+	}
+});
+
+///// contact show ////// 
+
+$('.contact').click(function(){
+	$('.contact-box').toggle();
+});
+
+
+
+
+/// json //////
+var data;
+
+function edit(d){
+	let parseData = '';
+	console.log(d);
+	for(let i = 0; i < d.length; i++){
+		parseData += '<div class="card"><a href="'+ d[i].url +'"><div class="card-head"><span class="circle"></span><span class="title">'+ d[i].title +'</span></div><img src="img/'+ d[i].image +'" alt="'+ d[i].title +'"></a><div class="body"><p>'+ d[i].info +'</p><section class="time"><span class="circle"></span><span>'+ d[i].time +'</span></section></div></div>';
+	}
+
+	$('.collection').html(parseData);
+}
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function(){
+	if(this.readyState == 4 && this.status == 200){
+		data = JSON.parse(this.responseText);
+		edit(data);
 	}
 }
 
-
-//
-document.addEventListener('scroll', function(){
-	if(document.documentElement.scrollTop > collection.offsetTop){
-		toggle(navbar,'on');
-	}
-	if(document.documentElement.scrollTop < collection.offsetTop){
-		toggle(navbar, 'remove');
-	}
-});
+xhr.open('GET','http://localhost/json/data.json',true);
+xhr.send();
 
 
-document.addEventListener('click', function(event){
-	if(event.target.className == 'contact'){
-		if(click.contact == false){
-			toggle(contactBox, 'on');
-			click.contact = true;
-		}else{
-			toggle(contactBox, 'remove')
-			click.contact = false;
-		}
-	}
-
-});
 
